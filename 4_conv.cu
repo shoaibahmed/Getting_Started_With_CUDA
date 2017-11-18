0a54019b14fbfc0acf4952a824bc91486fc812c7
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2017-11-18 18:44:00 +0100

    Bug fix

--- 4_conv.cu
+++ 4_conv.cu
@@ -4,8 +4,8 @@
 // Kernel addition on GPU
 __global__ void conv (float** I, float** F, int H, int W, int K, int filterPad, float** out)
 {
-	int m = threadIdx.x;
-	int n = threadIdx.y;
+	int m = threadIdx.y; // First dim
+	int n = threadIdx.x; // Second dim
 
 	float convOut = 0.0f;
 	for (int i = 0; i < K; i++)
@@ -27,7 +27,6 @@ __global__ void conv (float** I, float** F, int H, int W, int K, int filterPad, 
 			else
 			{
 				convOut += I[indI][indJ] * F[i][j]; // Correlation
-				// convOut += I[m + filterPad - i][n + filterPad - j] * F[i][j]; // Convolution
 			}
 		}
 	}
@@ -63,7 +62,7 @@ int main()
 
 	float **I, **dev_I;
 	float **F, **dev_F;
-	float **dev_Out;
+	float **out, **dev_Out;
 
 	printf ("Allocating memory on Host\n");
 	I = (float **) malloc(H * sizeof(float *));
@@ -73,31 +72,76 @@ int main()
 	F = (float **) malloc(K * sizeof(float *));
 	for (int i = 0; i < K; i++)
 		F[i] = (float*) malloc(sizeof(float) * K);
+
+	out = (float **) malloc(H * sizeof(float *));
+	for (int i = 0; i < H; i++)
+		out[i] = (float*) malloc(sizeof(float) * W);
 	printf ("Host memory allocated\n");
 
 	printf ("Initializing array\n");
 	init(I, F, H, W, K, filterPad); // Initialize the array
 	printf ("Initialization complete\n");
 
-	printf ("Allocating device memory\n");
-	cudaMalloc((void **) &dev_I, sizeof(float *) * H);
+	printf ("Allocating device memory for I\n");
+	cudaError_t err = cudaMalloc((void **) &dev_I, sizeof(float *) * H);
+	if(err != cudaSuccess)
+	{
+		printf("Failure in allocating I\n");
+		exit(1);
+	}
 	for (int i = 0; i < H; i++)
-		cudaMalloc((void **) &dev_I[i], sizeof(float) * W);
+	{
+		// cudaMalloc((void **) &dev_I[i], sizeof(float) * W);
+		err = cudaMalloc((void **) &I[i], sizeof(float) * W);
+		if(err != cudaSuccess)
+		{
+			printf("Failure in populating I\n");
+			exit(1);
+		}
+	}
 
-	cudaMalloc((void **) &dev_F, sizeof(float *) * K);
+	printf ("Allocating device memory for F\n");
+	err = cudaMalloc((void **) &dev_F, sizeof(float *) * K);
+	if(err != cudaSuccess)
+	{
+		printf("Failure in allocating F\n");
+		exit(1);
+	}
 	for (int i = 0; i < K; i++)
-		cudaMalloc((void **) &dev_F[i], sizeof(float) * K);
+	{
+		err = cudaMalloc((void **) &F[i], sizeof(float) * K);
+		if(err != cudaSuccess)
+		{
+			printf("Failure in populating K\n");
+			exit(1);
+		}
+	}
 
-	cudaMalloc((void **) &dev_Out, sizeof(float *) * H);
+	printf ("Allocating device memory for output\n");
+	err = cudaMalloc((void **) &dev_Out, sizeof(float *) * H);
+	if(err != cudaSuccess)
+	{
+		printf("Failure in allocating output array\n");
+		exit(1);
+	}
 	for (int i = 0; i < H; i++)
-		cudaMalloc((void **) &dev_Out[i], sizeof(float) * W);
+	{
+		err = cudaMalloc((void **) &out[i], sizeof(float) * W);
+		if(err != cudaSuccess)
+		{
+			printf("Failure in populating ouput array\n");
+			exit(1);
+		}
+	}
 	printf ("Device memory allocated\n");
 
 	printf ("Moving data to device\n");
-	for (int i = 0; i < H; i++)
-		cudaMemcpy(dev_I[i], I[i], sizeof(float) * W, cudaMemcpyHostToDevice);
-	for (int i = 0; i < K; i++)
-		cudaMemcpy(dev_F[i], F[i], sizeof(float) * K, cudaMemcpyHostToDevice);
+	// for (int i = 0; i < H; i++)
+	// 	cudaMemcpy(dev_I[i], I[i], sizeof(float) * W, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_I, I, sizeof(float *) * H, cudaMemcpyHostToDevice);
+	// for (int i = 0; i < K; i++)
+	// 	cudaMemcpy(dev_F[i], F[i], sizeof(float) * K, cudaMemcpyHostToDevice);
+	cudaMemcpy(dev_F, F, sizeof(float *) * K, cudaMemcpyHostToDevice);
 	printf ("Data moved to device\n");
 
 	printf ("Performing convolution\n");
@@ -105,8 +149,9 @@ int main()
 	printf ("Kernel sucessfully executed!\n");
 
 	printf ("Moving data back to host\n");
-	for (int i = 0; i < H; i++)
-		cudaMemcpy(I[i], dev_Out[i], sizeof(float) * W, cudaMemcpyDeviceToHost);
+	// for (int i = 0; i < H; i++)
+	// 	cudaMemcpy(I[i], dev_Out[i], sizeof(float) * W, cudaMemcpyDeviceToHost);
+	cudaMemcpy(out, dev_Out, sizeof(float *) * H, cudaMemcpyDeviceToHost);
 	printf ("Data moved to host\n");
 
 	cudaFree(dev_I);
@@ -114,12 +159,12 @@ int main()
 	cudaFree(dev_Out);
 	printf ("Device memory released\n");
 
-	print ("Convolution output:\n")
+	printf ("Convolution output:\n");
 	for (int i = 0; i < H; i++)
 	{
 		for (int j = 0; j < W; j++)
 		{
-			printf ("%.2f\t", I[i][j]);
+			printf ("%.2f\t", out[i][j]);
 		}
 		printf ("\n");
 	}
