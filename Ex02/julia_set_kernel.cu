2e32e6d7b50c21c074840626fd9f7361867ea77f
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2017-12-05 21:00:15 +0100

    Updated Julia set kernel

--- Ex02/julia_set_kernel.cu
+++ Ex02/julia_set_kernel.cu
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <assert.h>
-// #include <math.h>
 
 #define MAX_THREADS_PER_BLOCK 1024
 #define NUM_ITERATIONS 300
 
+#define ANIMATE_SET true
+
+#if ANIMATE_SET == true
 #define CONSTANT_1 -0.7885
 #define CONSTANT_2 0.7885
+#else
+#define CONSTANT_1 -0.8
+#define CONSTANT_2 0.156
+#endif
 
-//__global__ void JuliaSetKernel(uchar3 *dary, float* real, float* imaginary, int t, int DIMX, int DIMY, int numBlocksWithSameColor)
+#define SCALE 2.0
+#define SHIFT 1.0
+
 __global__ void JuliaSetKernel(uchar3 *dary, int t, int DIMX, int DIMY)
 {
 	/* Insert your kernel here */
@@ -16,26 +24,19 @@ __global__ void JuliaSetKernel(uchar3 *dary, int t, int DIMX, int DIMY)
 	int offset = blockIdx.x * blockDim.x + threadIdx.x;
 	
 	// Initialize the value of z using the initial c
-	float z_realPart = ((threadIdx.x / ((float)blockDim.x)) * 2.0) - 1.0;
-	float z_imaginaryPart = ((blockIdx.x / ((float)gridDim.x)) * 2.0) - 1.0;
-	// printf ("Real: %f | Imaginary: %f | Offset: %d\n", z_realPart, z_imaginaryPart, offset);
+	float z_realPart = ((threadIdx.x / ((float)blockDim.x)) * SCALE) - SHIFT;
+	float z_imaginaryPart = ((blockIdx.x / ((float)gridDim.x)) * SCALE) - SHIFT;
 
-	// Add C in Zn
-	// z_realPart = z_realPart - 0.8;
-	// z_imaginaryPart = z_imaginaryPart + 0.156;
-
-	// Perform 300 iterations
-	// float A;
-	// if ((t / 100) % 2 == 0)
-	// 	A = 0.08 + (t % 100) / 1220.0;
-	// else
-	// 	A = 0.08 + (99 - (t % 100)) / 1220.0;
-	float A = 0.08 + (t % 100) / 1220.0;
-
-	// Euler formula (cos(A) + i sin(A))
+#if ANIMATE_SET == true
+	float A = 0.08 + ((t % 100) / 1220.0);
+	
+	// Euler formula e^iA = (cos(A) + i sin(A))
 	float eulerReal = CONSTANT_1 * cos(A);
 	float eulerImaginary = CONSTANT_2 * sin(A);
-	// printf ("Euler Real: %f | Euler Imaginary: %f\n", eulerReal, eulerImaginary);
+#else
+	float eulerReal = CONSTANT_1;
+	float eulerImaginary = CONSTANT_2;
+#endif
 
 	for (int time = 0; time < NUM_ITERATIONS; time++)
 	{
@@ -43,16 +44,17 @@ __global__ void JuliaSetKernel(uchar3 *dary, int t, int DIMX, int DIMY)
 		// Complex multiplication: (x + yi)(u + vi) = (xu - yv) + (xv + yu)i
 		float z_realPart_new = ((z_realPart * z_realPart) - (z_imaginaryPart * z_imaginaryPart)) + eulerReal;
 		float z_imaginaryPart_new = ((z_realPart * z_imaginaryPart) + (z_realPart * z_imaginaryPart)) + eulerImaginary;
-
+		
 		// Update the values
 		z_realPart = z_realPart_new;
 		z_imaginaryPart = z_imaginaryPart_new;
-		// printf ("Real: %f | Imaginary: %f | Offset: %d\n", z_realPart, z_imaginaryPart, offset);
 	}
-	
-	// Determine if the value is different from others	
-	float zNormSquared = pow(z_realPart, 2) + pow(z_imaginaryPart, 2);
 
+	// Determine if the value is different from others	
+	//float zNormSquared = pow(z_realPart, 2) + pow(z_imaginaryPart, 2);
+	float zNormSquared = (z_realPart * z_realPart) + (z_imaginaryPart * z_imaginaryPart);
+
+	// Add the corresponding color to the pixel
 	uchar3 color;
 	if (zNormSquared < 2.0)
 		color = make_uchar3(255, 0, 0);
@@ -77,7 +79,7 @@ void simulate(uchar3 *ptr, int tick, int w, int h)
 
 	cudaEventRecord(start);
 
-	JuliaSetKernel<<<w, h>>>(ptr, tick, w, h);
+	JuliaSetKernel<<<w, h>>>(ptr, tick, h, w);
 	
 	err=cudaGetLastError();
 	if(err!=cudaSuccess) {
