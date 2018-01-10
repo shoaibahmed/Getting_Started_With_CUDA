588b6b2b5028cb11de5aa33e971bb3b7e5179ad5
Shoaib Ahmed Siddiqui <m.shoaib_ahmed@hotmail.com>
2018-01-10 20:03:53 +0100

    Added Ex04

---
+++ Ex04/Framework_GL.cpp
@@ -0,0 +1,123 @@
+#ifdef _WIN64
+#define GLUT_NO_LIB_PRAGMA
+#pragma comment (lib, "opengl32.lib")  /* link with Microsoft OpenGL lib */
+#pragma comment (lib, "glut64.lib")    /* link with Win64 GLUT lib */
+#pragma comment (lib, "winmm.lib")
+//_WIN64
+#include <Windows.h>
+#endif
+
+#include <GL/glew.h>
+#include <GL/glut.h>
+#include <GL/glext.h>
+#include <stdio.h>
+/*
+#include "Utils.h"
+*/
+#include "cuda.h"
+#include "cuda_runtime_api.h"
+#include "cuda_gl_interop.h"
+
+GLuint  vbo;
+cudaGraphicsResource *resource;
+size_t  size;
+uchar3* ptr;
+int dimx,dimy;
+int tick=0,A_ex=0;
+
+void simulate(uchar3* ptr, int tick, int width, int height);
+
+void initialize(int *width, int *height) {
+	// initialize the dimension of the picture here
+	*width=800;
+	*height=800;
+}
+
+void key( unsigned char key, int x, int y )
+{
+    switch (key) {
+        case 27: // On Escape Key
+		default:
+            // Clean up OpenGL and CUDA ressources
+            cudaGraphicsUnregisterResource(resource);
+			glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, 0);
+            glDeleteBuffersARB(1, &vbo);
+            exit(0);
+    }
+}
+
+void display()
+{
+	// Map the device memory to CUDA ressource pointer
+	cudaGraphicsMapResources(1, &resource, NULL);
+    cudaGraphicsResourceGetMappedPointer((void**)&ptr, &size, resource);
+	printf("%d ist size bei %d \n",size,dimx*dimy);
+
+	// Execute the CUDA kernel
+	simulate(ptr, tick, dimx, dimy);
+	tick++;
+
+	// Unmap the ressource for visualization
+	cudaGraphicsUnmapResources(1, &resource, NULL);
+
+	// Draw the VBO as bitmap
+    glDrawPixels(dimx, dimy, GL_RGB, GL_UNSIGNED_BYTE, 0);
+
+	// Swap OpenGL buffers
+    glutSwapBuffers();
+}
+
+void generate(int w, int h)
+{
+	// Create standard OpenGL 1.5 Vertex Buffer Object
+	glDeleteBuffersARB(1, &vbo);
+	glGenBuffersARB(1, &vbo);
+	glBindBufferARB(GL_PIXEL_UNPACK_BUFFER_ARB, vbo);
+    glBufferDataARB(GL_PIXEL_UNPACK_BUFFER_ARB, w * h * 3, NULL, GL_DYNAMIC_DRAW_ARB);
+
+	// Register buffer for CUDA access
+	cudaGraphicsGLRegisterBuffer(&resource, vbo, cudaGraphicsMapFlagsNone);
+}
+
+void reshape(int w, int h)
+{
+	// Set global dimensions
+	dimx = w; dimy = h;
+
+	// Generate GL buffer and register VBO
+	generate(w, h);
+
+	// Reset the projection for resize.
+	glViewport(0,0,(GLsizei)w,(GLsizei)h);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(45.0, (GLfloat)w / (GLfloat)h, 0.1, 2000);
+	glMatrixMode(GL_MODELVIEW);
+}
+
+int main(int argc, char **argv)
+{
+	// Coose the CUDA / OpenGL device
+	cudaGLSetGLDevice(0);
+
+	// Initialize OpenGL over GLUT
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	// Initialize by user
+	initialize(&dimx, &dimy);
+    glutInitWindowSize(dimx, dimy);
+    glutCreateWindow("CUDA Exercise");
+	// Extensions initialization
+	glewInit();
+
+	// Generate GL buffer and register VBO
+    generate(dimx, dimy);
+
+
+    // Register callbacks and start the GLUT render loop
+    glutKeyboardFunc(key);
+    glutDisplayFunc(display);
+	glutIdleFunc(glutPostRedisplay);
+	glutReshapeFunc(reshape);
+    glutMainLoop();
+}
---
+++ Ex04/simulation_kernel.cu
@@ -0,0 +1,98 @@
+#include <stdio.h>
+#include <assert.h>
+
+#include "curand.h"
+
+#define MAX_THREADS_PER_BLOCK 1024
+//#define NUM_ELEMENTS (800 * 800 * 2)
+
+int* d_particlePosition = NULL;
+float* d_particleVelocity = NULL;
+
+#define ACCELERATION -9.81
+
+__global__ void SimulationKernel(uchar3 *dary, float* particlePosition, float* particleVelocity, int t, int DIMX, int DIMY)
+{
+	// Since the array is ordered in WHC format
+	int offset = blockIdx.x * blockDim.x + threadIdx.x;
+
+	// Assign the colors only to the corresponding threads with a partcile position
+	int posX = 0;
+	int posY = 0;
+	int withinRange = 0;
+	if ((offset * 2) < NUM_ELEMENTS)
+	{
+		withinRange = 1;
+		posX = (int)(DIMX * particlePosition[offset*2]);
+		posY = (int)(DIMY * particlePosition[(offset*2)+1]);
+	}
+
+	// Add the corresponding color to the pixel
+	uchar3 color;
+	if (withinRange)
+	{
+		color = make_uchar3(255, 0, 0);
+	}
+	else
+	{
+		color = make_uchar3(0, 0, 0);
+	}
+	if
+	int realOffset = (posX * DIMX) + posY;
+	dary[realOffset] = color;
+}
+
+void simulate(uchar3 *ptr, int tick, int w, int h)
+{
+	/* ptr is a pointer to an array of size w*h*sizeof(uchar3).
+	   uchar3 is a structure with x,y,z coordinates to contain
+	   red,yellow,blue - values for a pixel (Range [0,255])
+	*/
+	cudaError_t err=cudaSuccess;
+	cudaEvent_t start,stop;
+	float elapsedtime;
+
+	// Initialize the data structures if not yet initialized
+	if (d_particlePosition == NULL)
+	{
+		err = cudaMalloc(&d_particlePosition, sizeof(float) * (800 * 800));
+		if (err != cudaSuccess)
+		{
+			printf("Error: Failed to allocate memory for particle position.");
+			exit(-1);
+		}
+
+		err = cudaMalloc(&d_particleVelocity, sizeof(float) * (800 * 800));
+		if (err != cudaSuccess)
+		{
+			printf("Error: Failed to allocate memory for particle velocity.");
+			exit(-1);
+		}
+
+		// Create the random grid
+		int positonGrid[800, 800];
+
+	}
+
+	cudaEventCreate  ( &start);
+	cudaEventCreate  ( &stop);
+
+	cudaEventRecord(start);
+
+	SimulationKernel<<<w, h>>>(ptr, d_particlePosition, d_particleVelocity, tick, h, w);
+
+	err=cudaGetLastError();
+	if(err!=cudaSuccess) {
+		fprintf(stderr,"Error executing the kernel - %s\n", cudaGetErrorString(err));
+		exit(EXIT_FAILURE);
+	}
+	cudaEventRecord(stop);
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&elapsedtime, start, stop);
+	printf("Time used: %.1f (ms)\n",elapsedtime);
+
+	cudaEventDestroy  ( start);
+	cudaEventDestroy  ( stop);
+
+	printf("Please type ESC in graphics and afterwards RETURN in cmd-screen to finish\n");
+}
