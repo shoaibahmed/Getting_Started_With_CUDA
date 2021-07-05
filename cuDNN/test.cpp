a3f6814b548585f131e80f7163dc2178a2d54df1
Muhammad.Shoaib.Ahmed Siddiqui <shoaib_ahmed.siddiqui@dfki.de>
2021-07-05 18:31:29 +0200

    Convolution algorithm implementation added

--- cuDNN/test.cpp
+++ cuDNN/test.cpp
@@ -27,6 +27,24 @@ cv::Mat load_image(const char *image_path)
   return image;
 }
 
+cv::Mat save_image(const char *output_filename,
+                   float *buffer,
+                   int height,
+                   int width)
+{
+  cv::Mat output_image(height, width, CV_32FC3, buffer);
+  // Make negative values zero.
+  cv::threshold(output_image,
+                output_image,
+                /*threshold=*/0,
+                /*maxval=*/0,
+                cv::THRESH_TOZERO);
+  cv::normalize(output_image, output_image, 0.0, 255.0, cv::NORM_MINMAX);
+  output_image.convertTo(output_image, CV_8UC3);
+  cv::imwrite(output_filename, output_image);
+  return output_image;
+}
+
 int main(int argc, char const *argv[])
 {
   // Initialize cudnn
@@ -94,26 +112,26 @@ int main(int argc, char const *argv[])
   checkCUDNN(cudnnGetConvolutionForwardAlgorithmMaxCount(cudnn, &requestedAlgoCount));
   std::vector<cudnnConvolutionFwdAlgoPerf_t> results(requestedAlgoCount);
   checkCUDNN(cudnnFindConvolutionForwardAlgorithm(cudnn,
-      input_descriptor, kernel_descriptor, convolution_descriptor, output_descriptor,
-      requestedAlgoCount,
-      &returnedAlgoCount,
-      &results[0]));
+                                                  input_descriptor, kernel_descriptor, convolution_descriptor, output_descriptor,
+                                                  requestedAlgoCount,
+                                                  &returnedAlgoCount,
+                                                  &results[0]));
 
   bool found_conv_algorithm = false;
   cudnnConvolutionFwdAlgo_t convolution_algorithm;
   size_t workspace_bytes = 0;
-  size_t free_memory, total_memory;
+  size_t free_memory = 1024 * 1024 * 1024; // 1 GB limit
   for (int i = 0; i < returnedAlgoCount; i++)
   {
-      if (results[i].status == CUDNN_STATUS_SUCCESS &&
-          results[i].algo != CUDNN_CONVOLUTION_FWD_ALGO_WINOGRAD_NONFUSED &&
-          results[i].memory < free_memory)
-      {
-          found_conv_algorithm = true;
-          convolution_algorithm = results[i].algo;
-          workspace_bytes = results[i].memory;
-          break;
-      }
+    if (results[i].status == CUDNN_STATUS_SUCCESS &&
+        results[i].algo != CUDNN_CONVOLUTION_FWD_ALGO_WINOGRAD_NONFUSED &&
+        results[i].memory < free_memory)
+    {
+      found_conv_algorithm = true;
+      convolution_algorithm = results[i].algo;
+      workspace_bytes = results[i].memory;
+      break;
+    }
   }
   assert(found_conv_algorithm);
 
@@ -128,7 +146,7 @@ int main(int argc, char const *argv[])
                                                  CUDNN_CONVOLUTION_FWD_PREFER_FASTEST,
                                                  /*memoryLimitInBytes=*/0,
                                                  &convolution_algorithm));
-  
+
   // Compute memory requirements for the operation
   size_t workspace_bytes = 0;
   checkCUDNN(cudnnGetConvolutionForwardWorkspaceSize(cudnn,
@@ -139,12 +157,13 @@ int main(int argc, char const *argv[])
                                                      convolution_algorithm,
                                                      &workspace_bytes));
 #endif
-  
+
   // Allocate the required memory for the algorithm
   std::cerr << "Workspace size: " << (workspace_bytes / 1048576.0) << "MB" << std::endl;
   void *d_workspace{nullptr};
   cudaMalloc(&d_workspace, workspace_bytes);
 
+  // Copy image data to the GPU
   int batch_size = 1, channels = 3, height = image.rows, width = image.cols;
   int image_bytes = batch_size * channels * height * width * sizeof(float);
 
@@ -155,4 +174,65 @@ int main(int argc, char const *argv[])
   float *d_output{nullptr};
   cudaMalloc(&d_output, image_bytes);
   cudaMemset(d_output, 0, image_bytes);
+
+  // Define a custom kernel
+  const float kernel_template[3][3] = {
+      {1, 1, 1},
+      {1, -8, 1},
+      {1, 1, 1}};
+
+  float h_kernel[3][3][3][3];
+  for (int kernel = 0; kernel < 3; ++kernel)
+  {
+    for (int channel = 0; channel < 3; ++channel)
+    {
+      for (int row = 0; row < 3; ++row)
+      {
+        for (int column = 0; column < 3; ++column)
+        {
+          h_kernel[kernel][channel][row][column] = kernel_template[row][column];
+        }
+      }
+    }
+  }
+
+  float *d_kernel{nullptr};
+  cudaMalloc(&d_kernel, sizeof(h_kernel));
+  cudaMemcpy(d_kernel, h_kernel, sizeof(h_kernel), cudaMemcpyHostToDevice);
+
+  // Perform the actual convolution operation
+  const float alpha = 1, beta = 0;
+  checkCUDNN(cudnnConvolutionForward(cudnn,
+                                     &alpha,
+                                     input_descriptor,
+                                     d_input,
+                                     kernel_descriptor,
+                                     d_kernel,
+                                     convolution_descriptor,
+                                     convolution_algorithm,
+                                     d_workspace,
+                                     workspace_bytes,
+                                     &beta,
+                                     output_descriptor,
+                                     d_output));
+
+  float *h_output = new float[image_bytes];
+  cudaMemcpy(h_output, d_output, image_bytes, cudaMemcpyDeviceToHost);
+
+  cv::Mat convertedImg = save_image("cudnn-out.png", h_output, height, width);
+  cv::imshow("Output", convertedImg);
+  cv::waitKey(-1);
+
+  delete[] h_output;
+  cudaFree(d_kernel);
+  cudaFree(d_input);
+  cudaFree(d_output);
+  cudaFree(d_workspace);
+
+  cudnnDestroyTensorDescriptor(input_descriptor);
+  cudnnDestroyTensorDescriptor(output_descriptor);
+  cudnnDestroyFilterDescriptor(kernel_descriptor);
+  cudnnDestroyConvolutionDescriptor(convolution_descriptor);
+
+  cudnnDestroy(cudnn);
 }
